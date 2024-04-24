1b2b91e7835e9ae34b15c1edb128fcb1ef14efba
LucasGomez2004 <lucas_gomezmaldonado@iescarlesvallbona.cat>
2024-04-24 16:56:59 +0200

    modificado import

--- scapeRoom-proyectoInicial/resources/views/import.blade.php
+++ scapeRoom-proyectoInicial/resources/views/import.blade.php
@@ -1,26 +1,26 @@
 @extends('adminlte::page')
 
 @section('content')
-<div class="row mt-2">
+<br>
+<br>
+<div class="row mt-2 ">
     <div class=" col-md-2 col-sm-12 "></div>
     <div class="card  col-md-8 col-sm-12">
+      <br>
       <h1 class="card-title text-center">Import CSV</h1>
       <div class="card-body">
-        <form class="row g-3" action="{{route('import')}}" method="POST" enctype="multipart/form-data">
+        <br>
+        <form class="row g-3" action="{{route('import')}}" method="POST" enctype="multipart/form-data" style="justify-content: center;">
             @csrf
-         
-          <div class="col-md-8">
-            <label for="file" class="form-label">Upload CSV</label>
+          <div class="col-md-8" >
             <input type="file" class="form-control  @error('file') is-invalid @enderror" accept="image/*" name="file" id="file" accept="application/vnd.ms-excel" onchange="document.getElementById('preview').src = window.URL.createObjectURL(this.files[0])">
             @error('file')
             <span class="text-danger">{{$message}}</span>
             @enderror
+            <br><br>
           </div>
-          <div class="col-md-4">
-            <img src="default.png" id="preview" class="img img-thumbnail" width="100">
-          </div>
-          <div class="col-12">
-            <button type="submit" class="btn btn-primary">Import</button>
+          <div class="col-12 text-center" >
+            <button type="submit" class="btn btn-danger">Import</button>
           </div>
         </form>
       </div>
