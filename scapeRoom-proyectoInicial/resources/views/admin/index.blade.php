ec35504c68edf23364c3aa8a968f8f4cdbd107a7
OscarGomez04 <oscar_gomezfiguls@iescarlesvallbona.cat>
2024-04-30 19:16:16 +0200

    DASHBOARD INICIO

--- scapeRoom-proyectoInicial/resources/views/admin/index.blade.php
+++ scapeRoom-proyectoInicial/resources/views/admin/index.blade.php
@@ -3,11 +3,23 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1 class='text-center'><b>ESCAPE OR DIE</b></h1>
+    <br>
 @stop
 
 @section('content')
-    <h5 class="text-center">¡Bienvenido!&nbsp<b>{{Auth::user()->name}}</b></h5>
+<div class="card card-widget widget-user">
+
+<div class="widget-user-header text-white" style="background: url('https://images.unsplash.com/photo-1571622390267-a277517fe908?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') center left; background-size:cover;">
+<h3 class="widget-user-username"><b>{{Auth::user()->name}}</b></h3>
+        <h5 class="widget-user-desc">@isset(Auth::user()->role) {{ Auth::user()->role->nom() }} @endisset</h5>
+</div>
+<div class="widget-user-image">
+<img class="img-circle" src="{{Auth::user()->avatar}}" alt="User Avatar">
+</div>
+<div class="card-footer">
+<div class="row">
+</div>
+</div>
 @stop
 
 @section('css')
