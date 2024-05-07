5a68b3c592ab03a50f6fc8c06fbd18cc61b9f093
LucasGomez2004 <lucas_gomezmaldonado@iescarlesvallbona.cat>
2024-05-07 16:38:56 +0200

    cambio de imagen

--- scapeRoom-proyectoInicial/resources/views/admin/index.blade.php
+++ scapeRoom-proyectoInicial/resources/views/admin/index.blade.php
@@ -16,7 +16,7 @@
 ?>
 
 <div class="card card-widget widget-user">
-    <div class="widget-user-header text-white" style="background: url('https://images.unsplash.com/photo-1571622390267-a277517fe908?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') center left; background-size:cover;">
+    <div class="widget-user-header text-white" style="background: url('https://images.unsplash.com/photo-1446104838475-bc6508184f08?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') center center; background-size:cover;">
         <h3 class="widget-user-username"><b>{{Auth::user()->name}}</b></h3>
         <h5 class="widget-user-desc">@isset(Auth::user()->role) {{ Auth::user()->role->nom() }} @endisset</h5>
     </div>
