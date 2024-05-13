a13058cc59ccf522c36456ec19e7994f27d60c34
OscarGomez04 <oscar_gomezfiguls@iescarlesvallbona.cat>
2024-05-13 18:45:23 +0200

    Login Personalizado Hecho

--- scapeRoom-proyectoInicial/resources/views/layouts/guest.blade.php
+++ scapeRoom-proyectoInicial/resources/views/layouts/guest.blade.php
@@ -1,30 +1,42 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="font-sans text-gray-900 antialiased">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                <img src="{{ asset('images/logo.webp') }}" class="w-24 h-30" alt="Logo de tu aplicación">    
-                </a>
-            </div>
-
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
+<div class="min-w-screen min-h-screen bg-light-900 flex items-center justify-center px-5 py-5">
+    <div class="bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-full overflow-hidden" style="max-width:1000px">
+        <div class="md:flex w-full">
+            <div style="background-image:url('{{ asset('images/inicio.gif') }}'); background-size:cover;" class="hidden md:block w-1/2 bg-indigo-500 py-10 px-10"></div>
+            <div class="w-full md:w-1/2 py-10 px-5 md:px-10">
+                <div class="mb-10">
+                    <br>
+                    {{ $slot }} 
+                </div>
             </div>
         </div>
-    </body>
+    </div>
+</div>
+
+<!-- BUY ME A BEER AND HELP SUPPORT OPEN-SOURCE RESOURCES -->
+<div class="flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10">
+    <div>
+        <a title="Repositorio del proyecto" href="https://github.com/LucasGomez2004/ProyectoM12" target="_blank" class="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12">
+            <img class="object-cover object-center w-full h-full rounded-full" src="{{ asset('images/logo.webp') }}"/>
+        </a>
+    </div>
+</div>
+
+</body>
 </html>
