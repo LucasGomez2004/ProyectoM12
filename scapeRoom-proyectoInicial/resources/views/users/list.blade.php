07d6be02e53cbe76a539de31959ee9a1b0d02a34
LucasGomez2004 <lucas_gomezmaldonado@iescarlesvallbona.cat>
2024-05-15 16:27:03 +0200

    filtrar rol hecho

--- scapeRoom-proyectoInicial/resources/views/users/list.blade.php
+++ scapeRoom-proyectoInicial/resources/views/users/list.blade.php
@@ -61,7 +61,6 @@ input[type=password] {
                         <form id="filterForm" action="{{ route('user.list') }}" method="GET" class="max-w-sm mx-auto">
                 <label for="filterRol" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Filtrar por Rol</label>
                 <select name="filterRol" id="filterRol" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                    <option value="">----</option>
                     <option value="0">Todos</option>
                     @foreach ($roles as $role)
                         <option value="{{ $role->name }}">{{ $role->name }}</option>
