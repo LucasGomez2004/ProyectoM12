c11ed58db58b6e1f544ae1b72a6e617d17f66333
bielzornoza <biel_zornozamoreno@iescarlesvallbona.cat>
2024-05-15 17:22:30 +0200

    Cambio en el list.blade.php (Reservas)

--- scapeRoom-proyectoInicial/resources/views/reservation/list.blade.php
+++ scapeRoom-proyectoInicial/resources/views/reservation/list.blade.php
@@ -28,7 +28,7 @@
 
     <div class="ml-auto">
                 <form id="filterForm" action="{{ route('reservation.list') }}" method="GET" class="max-w-sm mx-auto">
-                    <label for="filterLocalidad" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Filtrar por Localidad</label>
+                    <label for="filterLocalidad" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Filtrar por Localidad: </label>
                     <select name="filterLocalidad" id="filterLocalidad" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         <option value="0">Todos</option>
                         @foreach ($locations as $location)
